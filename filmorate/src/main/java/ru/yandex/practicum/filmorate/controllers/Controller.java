e6007f25bfcf7ed38729143407dda03ef99abc6b
Yaroslav <olddwarf3@gmail.com>
2023-07-22 02:43:20 +0600

    исправил переменую IdFilm на idFilms

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controllers/Controller.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controllers/Controller.java
@@ -11,18 +11,18 @@ import java.util.Map;
 
 @Slf4j
 public class Controller  {
-    private Map<Integer, IdFilm> IdFilms = new HashMap<>();
+    private Map<Integer, IdFilm> idFilms = new HashMap<>();
     private int nextId = 1;
 
     public IdFilm create(IdFilm idFilm) {
         idFilm.setId(nextId);
         nextId++;
-        return IdFilms.put(idFilm.getId(),idFilm);
+        return idFilms.put(idFilm.getId(),idFilm);
     }
 
     public IdFilm update(IdFilm idFilm) {
-        if (IdFilms.containsKey(idFilm.getId())) {
-            IdFilms.put(idFilm.getId(),idFilm);
+        if (idFilms.containsKey(idFilm.getId())) {
+            idFilms.put(idFilm.getId(),idFilm);
         } else {
             log.info("Фильм не обновлен, так как в хранилище нет фильмов с заданным id");
             throw new FilmObjectNotFoundException("Фильм с id: " + idFilm.getId() + "не найден");
@@ -31,6 +31,6 @@ public class Controller  {
     }
 
     List<IdFilm> getAll() {
-        return new ArrayList<>(IdFilms.values());
+        return new ArrayList<>(idFilms.values());
     }
 }
