1ee44cbe2ae12f52bb9b9202e9f584af519127c9
Yaroslav <olddwarf3@gmail.com>
2023-07-22 02:48:59 +0600

    исправил растояние в методe и фигурной скобки {

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmObjectNotFoundException.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmObjectNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class FilmObjectNotFoundException extends NullPointerException{
+public class FilmObjectNotFoundException extends NullPointerException {
     public FilmObjectNotFoundException(String message) {
         super(message);
     }
