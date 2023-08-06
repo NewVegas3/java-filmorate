41ee4f2f998e98cf5332f39b0cb232ce11e38491
Yaroslav <olddwarf3@gmail.com>
2023-08-07 00:20:48 +0600

    1.4

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
+
 @Component
 public class InMemoryUserStorage extends InMemoryStorage<User> {
 }
